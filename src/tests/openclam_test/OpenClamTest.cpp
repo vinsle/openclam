214257a1ca5d986676621fa162b523eeccd2cfac
vinsle <vinsle@gmail.com>
2010-02-26 17:37:02 +0000

    cleaning

--- src/tests/openclam_test/OpenClamTest.cpp
+++ src/tests/openclam_test/OpenClamTest.cpp
@@ -12,19 +12,18 @@
 
 namespace
 {
-    const unsigned int iNumElements = 100u;
     const float added = 3.f;
 
     const char * addKernel = KERNEL(
-        __kernel void VectorAdd( __global const float* a, __global const float* b, __global float* c, int iNumElements )
+        __kernel void VectorAdd( __global const float* a, __global const float* b, __global float* c, unsigned int iNumElements )
     {
-        const int iGID = get_global_id( 0 );
+        const unsigned int iGID = get_global_id( 0 );
         if( iGID >= iNumElements )
             return;
         c[ iGID ] = a[ iGID ] + b[ iGID ];
     } );
 
-    size_t shrRoundUp( int group_size, int global_size ) 
+    unsigned int shrRoundUp( unsigned int group_size, unsigned int global_size ) 
     {
         int r = global_size % group_size;
         if( r == 0 )
@@ -36,23 +35,13 @@ namespace
 BOOST_AUTO_TEST_CASE( test3 )
 {
     // set and log Global and Local work size dimensions
-    const size_t szLocalWorkSize = 256;
-    const size_t szGlobalWorkSize = shrRoundUp( szLocalWorkSize, iNumElements );  // rounded up to the nearest multiple of the LocalWorkSize
+    const unsigned int iNumElements = 100u;
+    const unsigned int szLocalWorkSize = 256;
+    const unsigned int szGlobalWorkSize = shrRoundUp( szLocalWorkSize, iNumElements );  // rounded up to the nearest multiple of the LocalWorkSize
 
     std::auto_ptr< float >srcA( new float[ szGlobalWorkSize ] );
     std::auto_ptr< float >srcB( new float[ szGlobalWorkSize ] );
     std::auto_ptr< float >dst( new float[ szGlobalWorkSize ] );
-
-    cl_context cxGPUContext;
-    cl_command_queue cqCommandQue;
-    cl_program cpProgram;
-    cl_kernel ckKernel;
-    cl_mem cmDevSrcA;
-    cl_mem cmDevSrcB;
-    cl_mem cmDevDst;
-    size_t szParmDataBytes;
-    size_t szKernelLength;
-    cl_int ciErr1, ciErr2;
 
     // Allocate and initialize host arrays 
     for( unsigned int i = 0; i < iNumElements; ++i )
@@ -61,32 +50,35 @@ BOOST_AUTO_TEST_CASE( test3 )
         srcB.get()[ i ] = added;
 
     // Create the OpenCL context on a GPU device
-    cxGPUContext = clCreateContextFromType( 0, CL_DEVICE_TYPE_GPU, NULL, NULL, &ciErr1 );
+    cl_int ciErr1, ciErr2;
+    cl_context cxGPUContext = clCreateContextFromType( 0, CL_DEVICE_TYPE_GPU, NULL, NULL, &ciErr1 );
 
     // Get the list of GPU devices associated with context
+    unsigned int szParmDataBytes;
     ciErr1 = clGetContextInfo( cxGPUContext, CL_CONTEXT_DEVICES, 0, NULL, &szParmDataBytes );
     std::auto_ptr< cl_device_id > cdDevices( new cl_device_id[ szParmDataBytes ] );
     ciErr1 |= clGetContextInfo( cxGPUContext, CL_CONTEXT_DEVICES, szParmDataBytes, cdDevices.get(), NULL );
 
     // Create a command-queue
-    cqCommandQue = clCreateCommandQueue( cxGPUContext, cdDevices.get()[ 0 ], 0, &ciErr1 );
+    cl_command_queue cqCommandQue = clCreateCommandQueue( cxGPUContext, cdDevices.get()[ 0 ], 0, &ciErr1 );
 
     // Allocate the OpenCL buffer memory objects for source and result on the device GMEM
-    cmDevSrcA = clCreateBuffer( cxGPUContext, CL_MEM_READ_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr1 );
-    cmDevSrcB = clCreateBuffer( cxGPUContext, CL_MEM_READ_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr2 );
+    cl_mem cmDevSrcA = clCreateBuffer( cxGPUContext, CL_MEM_READ_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr1 );
+    cl_mem cmDevSrcB = clCreateBuffer( cxGPUContext, CL_MEM_READ_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr2 );
     ciErr1 |= ciErr2;
-    cmDevDst = clCreateBuffer( cxGPUContext, CL_MEM_WRITE_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr2 );
+    cl_mem cmDevDst = clCreateBuffer( cxGPUContext, CL_MEM_WRITE_ONLY, sizeof(cl_float) * szGlobalWorkSize, NULL, &ciErr2 );
     ciErr1 |= ciErr2;
 
     // Create the program
+    unsigned int szKernelLength;
     szKernelLength = strlen( addKernel );
-    cpProgram = clCreateProgramWithSource( cxGPUContext, 1, (const char **)&addKernel, &szKernelLength, &ciErr1 );
+    cl_program cpProgram = clCreateProgramWithSource( cxGPUContext, 1, (const char **)&addKernel, &szKernelLength, &ciErr1 );
 
     // Build the program
     ciErr1 = clBuildProgram( cpProgram, 0, NULL, NULL, NULL, NULL );
 
     // Create the kernel
-    ckKernel = clCreateKernel( cpProgram, "VectorAdd", &ciErr1 );
+    cl_kernel ckKernel = clCreateKernel( cpProgram, "VectorAdd", &ciErr1 );
 
     // Set the Argument values
     ciErr1 = clSetKernelArg( ckKernel, 0, sizeof(cl_mem), (void*)&cmDevSrcA );
