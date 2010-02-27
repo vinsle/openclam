eebe7d5f32f5b0b4728dad9f1c1debabc09776b7
vinsle <vinsle@gmail.com>
2010-02-27 19:38:11 +0000

    add simple context test split test.h to fix build

---
+++ src/tests/openclam_test/context_test.cpp
@@ -0,0 +1,15 @@
+//
+//  Copyright Silvin Lubecki 2010
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "test.h"
+#include "openclam/cl.hpp"
+
+BOOST_AUTO_TEST_CASE( simple_context_instanciation )
+{
+    openclam::context context;
+}
