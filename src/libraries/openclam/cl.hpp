2f9920927e02fe88b5a32a9c8f240c7a344bbd1a
vinsle <vinsle@gmail.com>
2010-02-27 19:44:06 +0000

    cleaning

--- src/libraries/openclam/cl.hpp
+++ src/libraries/openclam/cl.hpp
@@ -20,15 +20,15 @@
 #define __global
 #define global
 
-#define APPLY_DEFINES( NAME, FUNCTION )     \
-class NAME_CLASS : public openclam::builtin \
-{                                           \
-private:                                    \
-             NAME_CLASS() {}                \
-    virtual ~NAME_CLASS() {}                \
-                                            \
-    FUNCTION;                               \
-};                                          \
+#define APPLY_DEFINES( NAME, FUNCTION )      \
+class NAME_CLASS : private openclam::builtin \
+{                                            \
+private:                                     \
+             NAME_CLASS() {}                 \
+    virtual ~NAME_CLASS() {}                 \
+                                             \
+    FUNCTION;                                \
+};                                           \
 
 #define KERNEL( NAME, FUNCTION ) \
 #FUNCTION;                       \
