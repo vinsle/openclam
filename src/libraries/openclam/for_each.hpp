6b5ea0ca0da42a1d656a67307cd7e7bfc04ade01
vinsle <vinsle@gmail.com>
2010-03-10 10:19:20 +0000

    cleaning

--- src/libraries/openclam/for_each.hpp
+++ src/libraries/openclam/for_each.hpp
@@ -10,6 +10,7 @@
 #define OPENCLAM_FOR_EACH_HPP_INCLUDED
 
 #include "kernel.hpp"
+#include <memory>
 
 namespace openclam
 {
@@ -17,14 +18,13 @@ namespace openclam
     void for_each( InputIterator first, InputIterator last, Kernel& k )
     {
         const unsigned int size = std::distance( first, last ); // $$$$ 2010-03-09 SILVIN: externalize these functions into openclam::buffer or vector object
-        InputIterator::value_type* data = new InputIterator::value_type[ size ];
+        std::auto_ptr< InputIterator::value_type > data( new InputIterator::value_type[ size ] );
         InputIterator it = first;
         for( unsigned int i = 0; it != last; ++it, ++i )
-            data[ i ] = *it;
-        k( data, size );
+            data.get()[ i ] = *it;
+        k( data.get(), size );
         for( unsigned int i = 0; first != last; ++first, ++i )
-            std::swap( *first, data[ i ] );
-        delete[]( data );
+            std::swap( *first, data.get()[ i ] );
     }
 }
 
