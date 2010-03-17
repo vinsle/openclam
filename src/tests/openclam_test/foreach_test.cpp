968f0cad9dda963f3ab9a82b1ba044a5223a8308
vinsle <vinsle@gmail.com>
2010-03-17 17:26:27 +0000

    remove std::for_each compliance, not realy that great

--- src/tests/openclam_test/foreach_test.cpp
+++ src/tests/openclam_test/foreach_test.cpp
@@ -41,33 +41,3 @@ BOOST_AUTO_TEST_CASE( for_each_with_unary_function_on_list_data )
     const std::list< unsigned int > expected = boost::assign::list_of( 4 ).repeat( 9, 4 );
     BOOST_CHECK( actual == expected );
 }
-
-BOOST_AUTO_TEST_CASE( use_of_stl_for_each_with_vector_data )
-{
-    std::vector< unsigned int > actual = boost::assign::list_of( 1 ).repeat( 9, 1 );
-    openclam::opencl wrapper;
-    openclam::context context( wrapper );
-    KERNEL( Unary, context, unsigned int,
-        kernel void Unary( __global unsigned int* a )
-        {
-            a[ get_global_id( 0 ) ] += 3;
-        } );
-    std::for_each( actual.begin(), actual.end(), openclam::unary_function< unsigned int, Unary_CLASS >( Unary ) );
-    const std::vector< unsigned int > expected = boost::assign::list_of( 4 ).repeat( 9, 4 );
-    BOOST_CHECK( actual == expected );
-}
-
-BOOST_AUTO_TEST_CASE( use_of_stl_for_each_with_list_data )
-{
-    std::list< unsigned int > actual = boost::assign::list_of( 1 ).repeat( 9, 1 );
-    openclam::opencl wrapper;
-    openclam::context context( wrapper );
-    KERNEL( Unary, context, unsigned int,
-        kernel void Unary( __global unsigned int* a )
-        {
-            a[ get_global_id( 0 ) ] += 3;
-        } );
-    std::for_each( actual.begin(), actual.end(), openclam::unary_function< unsigned int, Unary_CLASS >( Unary ) );
-    const std::list< unsigned int > expected = boost::assign::list_of( 4 ).repeat( 9, 4 );
-    BOOST_CHECK( actual == expected );
-}
